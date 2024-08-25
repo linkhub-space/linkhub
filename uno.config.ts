687048ac1dc78c3c3ead5313ec8af5975b88b35b
Sai Khay Khun Mong <saikhaykhunmong@gmail.com>
2024-08-25 15:29:11 +0700

    configs and about-us page

--- uno.config.ts
+++ uno.config.ts
@@ -3,4 +3,15 @@ import { defineConfig } from 'unocss'
 
 export default defineConfig({
   // ...UnoCSS options
+  theme : {
+    colors : {
+      primary : "#660066",
+      secondary : "#E93E3A",
+      accent : "#FFF33B",
+      basicGray : "#FFF33B",
+      basicPink : "#FFF1FF",
+      basicBlack : "#000",
+      basicWhite : "#fff",
+    }
+  }
 })
