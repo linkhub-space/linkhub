89680d44d57248b716600403d6c8fe14b21de5e3
wai-lin <dev.wailin@gmail.com>
2024-08-25 20:31:05 +0700

    feat: home page done

--- uno.config.ts
+++ uno.config.ts
@@ -1,17 +1,28 @@
 // uno.config.ts
-import { defineConfig } from 'unocss'
+import { defineConfig, presetUno, presetWebFonts, transformerVariantGroup } from 'unocss'
 
 export default defineConfig({
+  presets: [
+    presetUno(),
+    presetWebFonts({
+      fonts: {
+        sans: ["Merriweather Sans", "Inter"],
+      },
+    })
+  ],
+  transformers: [
+    transformerVariantGroup()
+  ],
   // ...UnoCSS options
-  theme : {
-    colors : {
-      primary : "#660066",
-      secondary : "#E93E3A",
-      accent : "#FFF33B",
-      basicGray : "#FFF33B",
-      basicPink : "#FFF1FF",
-      basicBlack : "#000",
-      basicWhite : "#fff",
+  theme: {
+    colors: {
+      primary: "#660066",
+      secondary: "#E93E3A",
+      accent: "#FFF33B",
+      basicGray: "#FFF33B",
+      basicPink: "#FFF1FF",
+      basicBlack: "#000",
+      basicWhite: "#fff",
     }
   }
 })
